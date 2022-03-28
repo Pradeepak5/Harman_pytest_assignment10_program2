6dceb252260e589a526f6799844a18a1d7cdca48
Pradeepak5 <pradeepak571@gmail.com>
2022-03-28 15:46:15 +0530

    pytesting assignment10  program 2 completed

---
+++ test_program2.py
@@ -0,0 +1,26 @@
+import program2
+import pytest
+
+def test_surface_area_prism():
+    l=2
+    b=3
+    h=4
+    result=program2.surface_area_prism(l,b,h)
+    assert result == 2*((l*b) + (l*h) + (b*h))
+
+def test_volume_prism():
+    l = 2
+    b = 3
+    h = 4
+    result = program2.volume_prism(l, b, h)
+    assert result == l*b*h
+
+def test_diagonol_prism():
+    l = 2
+    b = 3
+    h = 4
+    diagonal = ((l * l) + (b * b )+ (h * h))
+    result = program2.diagonol_prism(l, b, h)
+    assert result == diagonal ** 0.5
+
+
